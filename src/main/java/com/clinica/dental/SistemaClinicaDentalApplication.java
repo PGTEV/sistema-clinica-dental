d017703322246ca9aa1d2b2efe014c5ccf6eb961
Patrick Guillermo Espinoza Vera <76632682@continental.edu.pe>
2026-09-10 20:41:23 -0500

    Configuración inicial del proyecto

---
+++ src/main/java/com/clinica/dental/SistemaClinicaDentalApplication.java
@@ -0,0 +1,13 @@
+package com.clinica.dental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaClinicaDentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaClinicaDentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clinica/dental/SistemaClinicaDentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinica.dental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaClinicaDentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
